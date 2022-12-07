8f07afd36a77e2eee6fd3bd70b814f999fef1b20
michaldoman <mjd.domanski@gmail.com>
2022-12-07 16:35:38 +0100

    version_1.0

---
+++ Guessing game.py
@@ -0,0 +1,63 @@
+import random
+
+
+def main():
+    max_number = 100
+    rnd_number = random.randint(1, max_number)
+    number_of_attempts = 10
+    attempt = 0
+
+    starting_screen(number_of_attempts, max_number)
+
+    while attempt < number_of_attempts:
+        guess = input("What is your guess? ")
+        try:
+            guess = int(guess)
+        except ValueError:
+            print("That is not a number!!!")
+            print()
+            continue
+
+        if guess not in range(1, max_number):
+            print(f"That is not a valid guess. Type a number from 1 to {max_number}...")
+            print()
+            continue
+        else:
+            attempt += 1
+
+            if rnd_number == guess and attempt == 1:
+                print(f"Congratulations! It was {rnd_number}! It took you 1 attempt!")
+                print('')
+                break
+            elif rnd_number == guess and attempt > 1:
+                print(f"Congratulations! It was {rnd_number}! It took you {attempt} attempts!")
+                print('')
+                break
+            elif rnd_number < guess:
+                print("Sorry, that's too HIGH...")
+            else:
+                print("Sorry, that's too LOW...")
+
+        print(f'Attempts remaining: {number_of_attempts - attempt}')
+        print('')
+
+    if number_of_attempts == attempt:
+        print()
+        print("You loose! You're out of attempts.")
+
+    print("Thank you for playing!")
+
+
+def starting_screen(number_of_attempts, max_number):
+    # Generate an introduction
+    print("-----------------------------------------")
+    print("     Welcome to the Guessing Game!")
+    print("-----------------------------------------")
+
+    print(f"You have {number_of_attempts} tries to guess a randomly generated number, "
+          f"within the range of 0 and {max_number}!")
+    print()
+
+
+if __name__ == '__main__':
+    main()
